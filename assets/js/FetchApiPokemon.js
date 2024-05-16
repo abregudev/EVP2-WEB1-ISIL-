7cca8c6dd4b5fb45a2b14b04315ee2ee4af7cd9e
AbreguDev <abregudev@gmail.com>
2024-05-15 22:27:15 -0500

    second commit

---
+++ assets/js/FetchApiPokemon.js
@@ -0,0 +1,25 @@
+// Ingreso la API en un variable no modificable por buenas practicas.
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+// Creamos un evento donde al hacer click hacemos peticion del API POKEMON sumandole un numero ramdom de 1 al 152
+document.getElementById("getAPIpokemon").addEventListener("click", () => {
+    let datoNumeroRamdom = Math.floor(Math.random() * 152);
+    fetch(URL + datoNumeroRamdom)
+    .then((response) => response.json())
+    .then(data => {
+        document.getElementById("pokemonStat").innerHTML = 
+        `
+        <p class="pokemon-id">#${datoNumeroRamdom}</p>
+        <img class="imgPokemon" src="${data.sprites.front_default}"></img>
+        `
+        document.getElementById("pokemonStats").innerHTML = 
+        `
+         <h2>${data.species.name}</h2>
+                <div class="tipos">${data.types[0].type.name}</div>
+                <div class="col2">
+                    <p class="peso infopeso_altura">${data.weight}KG</p>
+                    <p class="altura infopeso_altura">${data.height}M</p>
+                </div>
+        `
+    })
+})
